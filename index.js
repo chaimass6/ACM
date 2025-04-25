730c2338aa587e9558e24c19adc1b6ac6dab7b3f
chaimass6 <chaitu.6914@gmail.com>
2025-04-25 23:40:25 +0530

    ok

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function handleSignup(event) {
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-analytics.js";
-  import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/ firebasejs/10.7.2/firebase-auth.js";
+  // import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/ firebasejs/10.7.2/firebase-auth.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
