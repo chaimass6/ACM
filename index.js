ad8856d6fa45d43c3567a3abf1ecb3ccc5201e47
chaimass6 <chaitu.6914@gmail.com>
2025-04-25 23:26:03 +0530

    ok

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ function handleSignup(event) {
   alert(`Signup successful for ${name}`);
   return false;
 }
-
+<script type="module">
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-analytics.js";
